7674412266492c93061f3adad8ec17a36636b5c6
Brenda Rayane <brenda.rayane.0018@gmail.com>
2023-01-05 15:10:57 -0300

    fix: post submitted and timeline updated

--- src/Pages/Timeline/TimelinePage.js
+++ src/Pages/Timeline/TimelinePage.js
@@ -12,9 +12,15 @@ import BoxPost from "../../Components/BoxPost";
 import Trending from "../../Components/Trending";
 
 export default function TimelinePage() {
-    const { posts, setPosts } = useContext(DadosContext);
+    const { posts, setPosts,  linkPost, setLinkPost, textPost, setTextPost } = useContext(DadosContext);
 
     useEffect(() => {
+        // const config = {
+        //     headers: {
+        //         "Authorization": `Bearer ${token}`
+        //     }
+        // }
+
         axios.get("http://localhost:4000/timeline-posts")
             .then((res) => {
                 setPosts(res.data);
@@ -24,24 +30,64 @@ export default function TimelinePage() {
             })
     }, [])
 
+    function publishPost(event){
+        event.preventDefault()
+        // const config = {
+        //     headers: {
+        //         "Authorization": `Bearer ${token}`
+        //     }
+        // }
+
+        const body = {texto: textPost, link: linkPost}
+
+        axios.post("http://localhost:4000/timeline-posts", body)
+        .then((res)=>{
+            console.log(res.data)
+            atualizarTimeline()
+        })
+        .catch((err)=>{
+            console.log(err.response.data)
+        })
+    }
+
+    function atualizarTimeline(){
+        axios.get("http://localhost:4000/timeline-posts")
+        .then((res) => {
+            setPosts(res.data);
+        })
+        .catch((err) => {
+            console.log(err.response.data)
+        })
+    }
+
     return (
         <ContainerTimeline>
             <Header />
             <ContainerPostsAndTrending>
                 <ContainerPosts>
                     <TittlePosts>timeline</TittlePosts>
-                    <BoxInputs>
-                        <Image><img src="https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782__340.jpg" alt="profile" /></Image>
-                        <ContainerInputs>
-                            <span>What are you going to share today?</span>
-                            <InputLink
-                                placeholder="http://..." />
-                            <InputText
-                                placeholder="Awesome article about #javascript" />
-                            <ButtonPost>Publish</ButtonPost>
-                        </ContainerInputs>
-                    </BoxInputs>
-                    {posts === "" ? "Carregando" : posts.map((p) => <BoxPost post={p} />)}
+                    <form onSubmit={publishPost}>
+                        <BoxInputs>
+                            <Image><img src="https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782__340.jpg" alt="profile" /></Image>
+                            <ContainerInputs>
+                                <span>What are you going to share today?</span>
+                                <InputLink
+                                    placeholder="http://..."
+                                    onChange={e => setLinkPost(e.target.value)}
+                                    value={linkPost}
+                                    type="url"
+                                    required />
+                                <InputText
+                                    placeholder="Talk about your link"
+                                    onChange={e => setTextPost(e.target.value)}
+                                    value={textPost}
+                                    type="text" />
+                                <ButtonPost type="submit">Publish</ButtonPost>
+                            </ContainerInputs>
+                        </BoxInputs>
+                    </form>
+                    {posts === "" ? "Carregando" :
+                        posts.length === 0 ? "There are no posts yet" : posts.map((p) => <BoxPost post={p} />)}
                 </ContainerPosts>
                 <Trending />
             </ContainerPostsAndTrending>
--- src/context/DadosContext.js
+++ src/context/DadosContext.js
@@ -6,12 +6,16 @@ export default function ContextProvider({ children }) {
     const [userName, setUserName] = useState("")
     const [userImg, setUserImg] = useState("")
     const [posts, setPosts] = useState("")
+    const [linkPost, setLinkPost] = useState("")
+    const [textPost, setTextPost] = useState("")
 
     return (
         <DadosContext.Provider value={{
             userName, setUserName,
             userImg, setUserImg,
-            posts, setPosts
+            posts, setPosts,
+            linkPost, setLinkPost,
+            textPost, setTextPost
         }}>
             {children}
         </DadosContext.Provider>
